0da5822f99d798b12b8fd8284e2e437e4c228f3f
scott2346413 <2346413@swansea.ac.uk>
2023-11-09 13:37:48 +0000

    Finished commenting code

--- Assets/ForestFire/Scripts/DamagePlayer.cs
+++ Assets/ForestFire/Scripts/DamagePlayer.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    [SerializeField] float damagePerSecond;
-
-    [SerializeField] GameObject damageSound;
-
-    float nextDamage;
+    [SerializeField] float damagePerSecond; // how much damage to deal each second
+    [SerializeField] GameObject damageSound; // object which plays damaging sound
 
     private void Start()
     {
-        nextDamage = Time.time;
-        damageSound.SetActive(false);
+        damageSound.SetActive(false); // set damage sound object to false
     }
 
+    //When anything enters the trigger
     private void OnTriggerEnter(Collider other)
     {
-        tryDamage(other);
+        tryDamage(other); // try to damage
     }
 
+    // When anything stays in the trigger
     private void OnTriggerStay(Collider other)
     {
-        tryDamage(other);
+        tryDamage(other); // try to damage
     }
 
+    // When anything leaves the trigger
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<PlayerHealth>() != null)
+        if (other.GetComponent<PlayerHealth>() != null) // if this is the player, deactivate the sound
         {
             damageSound.SetActive(false);
         }
     }
 
+    // Try Damage tries to damage the target collider, checking if it is the player
     void tryDamage(Collider target)
     {
-        PlayerHealth health = target.GetComponent<PlayerHealth>();
+        PlayerHealth health = target.GetComponent<PlayerHealth>(); // try to get the player health component
 
-        if (health == null)
+        if (health == null) // if health is null, return (not player)
         {
             return;
         }
 
-        health.doDamage(damagePerSecond * Time.deltaTime);
-        damageSound.SetActive(true);
+        health.doDamage(damagePerSecond * Time.deltaTime); // Do damage, scaled by deltaTime
+        damageSound.SetActive(true); // set damaging sound to active
     }
 
 }
--- Assets/ForestFire/Scripts/FadeAway.cs
+++ Assets/ForestFire/Scripts/FadeAway.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class FadeAway : MonoBehaviour
 {
-    [SerializeField] float fadeDelay;
-    [SerializeField] float fadeTime;
+    [SerializeField] float fadeDelay; // how long before object starts fading
+    [SerializeField] float fadeTime; // how long it takes to fade
 
-    float startFadingTime;
-    float destroyTime;
+    float startFadingTime; // when is object going to start fading
+    float destroyTime; // when should we destroy the object
 
-    MeshRenderer _renderer;
+    MeshRenderer _renderer; // the object's renderer
 
     // Start is called before the first frame update
     void Start()
     {
-        startFadingTime = Time.time + fadeDelay;
-        destroyTime = Time.time + fadeDelay + fadeTime;
+        startFadingTime = Time.time + fadeDelay; // set when to start fading
+        destroyTime = Time.time + fadeDelay + fadeTime; // set when to destroy
 
-        _renderer = GetComponent<MeshRenderer>();
+        _renderer = GetComponent<MeshRenderer>(); // get the renderer
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > startFadingTime)
+        if (Time.time > startFadingTime) // if the time to start fading has passed, start fading
         {
-            Color color = _renderer.material.color;
-            color.a = ((destroyTime-Time.time) / fadeTime) * 1f;
-            _renderer.material.color = color;
+            Color color = _renderer.material.color; // get material colour
+            color.a = ((destroyTime-Time.time) / fadeTime) * 1f; // set alpha to a percentage of how long is left to fade
+            _renderer.material.color = color; // set colour to renderer again
         }
 
-        if(Time.time > destroyTime)
+        if(Time.time > destroyTime) // if time to destroy has passed
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // destory the object once faded
         }
     }
 }
